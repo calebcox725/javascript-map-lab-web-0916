c324d92c9f417b1d622d8064c6514dcf399280ff
calebcox725 <caleb.cox@flatironschool.com>
2016-11-07 09:35:45 -0500

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,33 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null },
 ];
+
+var sortedRobots = robots.map((robot) => {
+  let alliance;
+
+  if (knownDecepticons.includes(robot.name)) {
+    alliance = 'decepticon';
+  } else {
+    alliance = 'autobot';
+  }
+
+  return Object.assign({}, robot, {
+    name: robot.name,
+    alliance: alliance
+  });
+});
+
+var coloredZebraStripes = zebraStripes.map((stripe, index) => {
+  let color;
+
+  if (index % 2 === 0) {
+    color = 'black';
+  } else {
+    color = 'white';
+  }
+
+  return Object.assign({}, stripe, {
+    width: stripe.width,
+    color: color
+  });
+});
